905be0e42b1a7fab788c7034c2243964be7f33f9
andrew-Woo9 <woo95kil@gmail.com>
2017-02-23 17:33:34 +0900

    login setting

---
+++ django_app/member/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from facebook import settings
+
+
+def login_fbv(request):
+    facebook_app_id = settings.config['facebook']['app_id']
+
+    context = {
+        'facebook_app_id': facebook_app_id,
+    }
+    return render(request, 'member/login.html', context)
+
+
+def login_facebook(request):
+    print(request.GET)
